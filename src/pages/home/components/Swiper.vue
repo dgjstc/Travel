6beb949db58767619126374f29a3049e65e07ffb
jiaxiaowei <jiaxiaowei_unity@hotmail.com>
2018-06-17 20:24:05 +0800

    'change'

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+       <img class="swiper-img" :src="item.imgUrl"/>
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/11/947d8b7e221cbb02.jpg_750x200_4145c399.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper>>>.swiper-pagination-bullet-active
+    background:#fff
+  .wrapper
+    overflow:hidden
+    width:100%
+    height:0
+    padding-bottom:26.6%
+    background:#eee
+  .swiper-img
+    width:100%
+</style>

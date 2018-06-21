a62f5550c263cb980c2065073fd14aaec466c894
jiaxiaowei <jiaxiaowei_unity@hotmail.com>
2018-06-21 23:25:06 +0800

    finish city css

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -16,13 +16,13 @@ export default{
     height:.72rem
     padding: 0 .1rem
     background:$bgColor
-  .search-input
-    box-sizing:border-box
-    width:100%
-    height:.62rem
-    padding: 0 .1rem
-    line-height:.62rem
-    tect-align:center
-    border-radius:.06rem
-    color:#666
+    .search-input
+       box-sizing:border-box
+       width:100%
+       height:.62rem
+       padding: 0 .1rem
+       line-height:.62rem
+       text-align:center
+       border-radius: .06rem
+       color:#666
 </style>
